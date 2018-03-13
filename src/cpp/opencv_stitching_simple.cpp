0ac8273e2e7a65848d7d8aaac24ce76346fff2a0
amusi <amusi1994@gmail.com>
2018-03-13 20:10:51 +0800

    添加opencv_stithcing_simple.cpp注释

--- src/cpp/opencv_stitching_simple.cpp
+++ src/cpp/opencv_stitching_simple.cpp
@@ -1,16 +1,16 @@
-// Summary: Use OpenCV library to complete simple image stitching
+// Summary: 对OpenCV库中的stitching.cpp修改和添加注释
 // Author:  Amusi
 // Date:    2018-03-01
 // Reference:
 //		[1]Stitcher class: https://docs.opencv.org/master/d2/d8d/classcv_1_1Stitcher.html
-//		[2]stitching.cpp: https://docs.opencv.org/master/d8/d19/tutorial_stitcher.html
+//		[2]stitching tutorial: https://docs.opencv.org/master/d8/d19/tutorial_stitcher.html
 
-// Note: not use GPU
+// Note: 此程序不使用GPU加速
 
-
-// C++ head
+// C++库文件
 #include<iostream>
-// OpenCV head
+
+// OpenCV库文件
 #include<opencv2/opencv.hpp>
 
 using namespace std;
@@ -19,48 +19,66 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	// load images
+	// 读取图像
 	vector<Mat> imgs;
 	Mat pano;
 	Mat img_temp;
 
+#if 1
+	// 命令行输入参数
+	if(argc < 2)
+	{
+		cerr << "Please input images name..." << endl;
+		cerr << "Such as: *.exe img1 img2 img3 ... " << endl;
+		return -1;
+	}
+	for(int i = 1; i < argc; ++i)
+	{
+		img_temp = imread(argv[i]);
+		imgs.push_back(img_temp);
+	}
+#else
+	// 固定参数
 	img_temp = imread("0001.jpg");
 	imgs.push_back(img_temp);
 	img_temp = imread("0002.jpg");
 	imgs.push_back(img_temp);
 	img_temp = imread("0003.jpg");
 	imgs.push_back(img_temp);
+#endif
 
-	// set Stitcher parameters
-	bool try_use_gpu = false;					// true: use GPU; false: use CPU
+	// 设置Stitcher参数
+	bool try_use_gpu = false;					// true: 使用GPU; false: 使用CPU
 	bool divide_images = true;					// true: false:
-	Stitcher::Mode mode = Stitcher::PANORAMA;	// Mode: PANORAMA=0 create panoramo; SCANS=1 create scan
+	Stitcher::Mode mode = Stitcher::PANORAMA;	// Mode: PANORAMA=0 创建全景图 panoramo; SCANS=1 创建扫描图
 
+	// 图像拼接计时
 	double time_start = static_cast<double>(getTickCount());
 
 #if 1
-	// create Stitcher instance with smart pointer
+	// 创建指向Stitcher类的智能指针Ptr
 	Ptr<Stitcher> stitcher = Stitcher::create(mode, try_use_gpu);
-	// stitch
+	// 图像拼接
 	Stitcher::Status status = stitcher->stitch(imgs, pano);
 
 #else
-	// create Stitcher instance
+	// 创建Stitcher类对象
 	Stitcher stitcher = Stitcher::createDefault(try_use_gpu);
-	// stitch
+	// 图像拼接
 	Stitcher::Status status = stitcher.stitch(imgs, pano);
 #endif
 
-	// judge the stitching status
+	// 判断拼接状态
 	if (status != Stitcher::OK)
 	{
 		cerr << "Can't stitch images, error code = " << static_cast<int>(status) << endl;
 		return -1;
 	}
 
-	cout << "Cost time: " << static_cast<double>((getTickCount()-time_start) / getTickFrequency()) << "s"<<endl;
+	// 计算图像拼接耗时
+	cout << "Cost time: " << static_cast<double>((getTickCount()-time_start) / getTickFrequency()) << " sec"<<endl;
 
-	// show and save panoramo
+	// 显示并保存全景图
 	namedWindow("Panoramo", WINDOW_NORMAL);
 	imshow("Panoramo", pano);
 	imwrite("panoramo.jpg", pano);
